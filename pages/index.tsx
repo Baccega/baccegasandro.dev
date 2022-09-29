97ceede1561043f861b2ba652b43cab513e95426
Sandro Baccega <mail.sandro.baccega@gmail.com>
2022-09-29 14:23:15 +0200

    🚧 Add WIP text

--- pages/index.tsx
+++ pages/index.tsx
@@ -70,6 +70,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Baccega Sandro Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1>WIP</h1>
       {props.map(({ x, y, rot, scale }, i) => (
         <animated.div
           key={i}

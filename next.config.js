03d25dcb87919116022b8fa288aa0c7fcc169a07
Sandro Baccega <mail.sandro.baccega@gmail.com>
2022-09-30 13:46:19 +0200

    ➕ Add Scss

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}

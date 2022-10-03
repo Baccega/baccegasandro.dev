cb4b6c0e0a5d46b52a803cd2be4214b188c37047
Sandro Baccega <mail.sandro.baccega@gmail.com>
2022-10-03 11:19:24 +0200

    💫 Update animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,17 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import buttonStyles from "../styles/Button3D.module.scss";
-import { animated, to, useSprings } from "react-spring";
+import { animated, to, useSprings, config } from "react-spring";
 import Card from "../components/Card";
 import { cards } from "../content/cards";
 import background from "../public/background.jpg";
 import Button3D from "../components/Button3D";
 import { MdArrowForward } from "react-icons/md";
 import { GiSandsOfTime } from "react-icons/gi";
+
+function randomIntFromInterval(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 const stacked_position = (i: number) => ({
   x: 0,
@@ -20,9 +24,9 @@ const stacked_position = (i: number) => ({
 });
 const up_position = (i: any) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
 const random_position = (i: any) => ({
-  x: Math.random() * 100,
-  y: Math.random() * 100,
-  rot: Math.random() * 90,
+  x: randomIntFromInterval(-200, 200),
+  y: randomIntFromInterval(-200, 200),
+  rot: randomIntFromInterval(-100, 100),
   scale: 1,
 });
 const trans = (r: number, s: any) =>
@@ -32,7 +36,7 @@ const trans = (r: number, s: any) =>
 
 const Home: NextPage = () => {
   const [position, setPosition] = React.useState(cards.length - 1);
-  const [props, set] = useSprings(cards.length, (i) => ({
+  const [props, api] = useSprings(cards.length, (i) => ({
     ...stacked_position(i),
     from: up_position(i),
   }));
@@ -40,29 +44,34 @@ const Home: NextPage = () => {
   const handleClick = (index: number) => (e: any) => {
     if (index >= 0) {
       setPosition(index - 1);
-      set((i) => {
+      api.start((i) => {
         if (index !== i) return;
         const direction = Math.random() > 0.5 ? 1 : -1;
         const x = (300 + window.innerWidth) * direction;
+        const y = randomIntFromInterval(
+          -window.innerHeight,
+          window.innerHeight
+        );
         const scale = 1.1;
         return {
           x,
+          y,
           scale,
-          rot: 100 * direction,
+          rot: 200 * direction,
           delay: undefined,
-          config: { friction: 50, tension: 200 },
+          config: config.slow,
         };
       });
       if (index === 0) {
         setTimeout(() => {
-          set((i) => random_position(i));
+          api.start((i) => random_position(i));
         }, 500);
         setTimeout(() => {
-          set((i) => up_position(i));
+          api.start((i) => up_position(i));
         }, 1500);
         setTimeout(() => {
           setPosition(cards.length - 1);
-          set((i) => stacked_position(i));
+          api.start((i) => stacked_position(i));
         }, 2000);
       }
     }
@@ -101,14 +110,12 @@ const Home: NextPage = () => {
         </animated.div>
       ))}
       <div className={buttonStyles.container}>
-        <Button3D disabled={position < 0} icon={position < 0? <GiSandsOfTime /> : <MdArrowForward />} text={position < 0? "Reshuffling" : "Next card"} onClick={handleClick(position)} />
-        {/* <Button3D
-          icon="ok"
-          text="test"
-          onClick={(e) => {
-            console.log("OK");
-          }}
-        /> */}
+        <Button3D
+          disabled={position < 0}
+          icon={position < 0 ? <GiSandsOfTime /> : <MdArrowForward />}
+          text={position < 0 ? "Reshuffling" : "Next card"}
+          onClick={handleClick(position)}
+        />
       </div>
     </div>
   );

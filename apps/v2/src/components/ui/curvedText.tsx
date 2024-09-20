38229d80c1ca35bdd08b62c7f31b8d9798f22fb9
Sandro Baccega <sandro.baccega@wirecube.at>
2024-09-20 10:14:00 +0200

    🎨 Improve curved text

--- apps/v2/src/components/ui/curvedText.tsx
+++ apps/v2/src/components/ui/curvedText.tsx
@@ -7,7 +7,7 @@ const curvedTextPathVariants = cva(
     {
         variants: {
             size: {
-                small: "text-[90px]",
+                small: "text-[85px]",
                 default: "text-[110px]",
             },
         },
@@ -22,8 +22,8 @@ const curvedGMatrixVariants = cva(
     {
         variants: {
             size: {
-                small: "matrix(1,0,0,1,-345,-220)",
-                default: "matrix(1,0,0,1,-350,-210)",
+                small: "matrix(1,0,0,1,-355,-270)",
+                default: "matrix(1,0,0,1,-355,-260)",
             },
         },
         defaultVariants: {
@@ -61,7 +61,7 @@ const CurvedText = React.forwardRef<HTMLOrSVGElement, CurvedTextProps>(
             <svg viewBox="0 0 1249 92" width="100%" height="75">
                 <title>{text}</title>
                 <g transform={cn(curvedGMatrixVariants({ size }))}>
-                    <path ref={pathRef} className="hidden" fill="red" id="curve" d="M353.698,331.744C776.993,204.83 1192.62,213.923 1602.18,330.416" />
+                    <path ref={pathRef} className="hidden" fill="red" id="curve" d="M353.698,398.093 C776.993,245.796 1192.62,256.708 1602.18,396.499" />
                     <text ref={textRef} className={cn(curvedTextPathVariants({ size }))}>
                         <textPath xlinkHref="#curve" startOffset={startOffset}>
                             {text}

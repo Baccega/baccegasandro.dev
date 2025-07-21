9f85922eb9688298d4edb636274c43c6897141f8
Sandro Baccega <sandro.baccega@chax.at>
2025-07-21 22:16:45 +0200

    🎨 Move badges to card side

--- apps/v2/src/components/ui/card.tsx
+++ apps/v2/src/components/ui/card.tsx
@@ -21,7 +21,7 @@ const cardWrapperVariants = cva(
 );
 
 const cardContainerVariants = cva(
-	"relative grid grid-rows-[1fr,80px,auto,1fr] grid-cols-[1.5rem,1fr,1.5rem] items-center justify-center rounded-2xl w-full h-full",
+	"relative grid grid-rows-[1fr,auto,80px,1fr] grid-cols-[1.5rem,1fr,1.5rem] items-center justify-center rounded-2xl w-full h-full",
 	{
 		variants: {
 			variant: {
@@ -52,42 +52,21 @@ const cardDescriptionVariants = cva(
 );
 
 const badgeVariants = cva(
-	"absolute border-[3px] bg-white border-card-dividers-color w-10 h-10 rounded-full flex justify-center items-center cursor-help",
+	"absolute right-[-18px] border-[3px] bg-white border-card-dividers-color w-10 h-10 rounded-full flex justify-center items-center cursor-help",
 	{
 		variants: {
 			position: {
-				center: "bottom-0",
-				'odd-1': "left-[65px] bottom-[-3px]",
-				'odd-3': "right-[65px] bottom-[-3px]",
-				'even-1': "left-[35px] bottom-[-8px]",
-				'even-2': "left-[90px] bottom-[-2px]",
-				'even-3': "right-[90px] bottom-[-2px]",
-				'even-4': "right-[35px] bottom-[-8px]",
+				1: "bottom-[130px]",
+				2: "bottom-[88px]",
+				3: "bottom-[46px]",
+				4: "bottom-[4px]",
 			},
 		},
+		defaultVariants: {
+			position: 1
+		}
 	},
 );
-
-function getBadgePosition(index: number, totalNumberOfBadges: number): VariantProps<typeof badgeVariants>["position"] {
-	switch (true) {
-		case totalNumberOfBadges === 1 || (totalNumberOfBadges === 3 && index === 1):
-			return 'center'
-		case totalNumberOfBadges === 3 && index === 0:
-			return 'odd-1';
-		case totalNumberOfBadges === 3 && index === 2:
-			return 'odd-3';
-		case totalNumberOfBadges === 4 && index === 0:
-			return 'even-1';
-		case totalNumberOfBadges === 2 && index === 0 || (totalNumberOfBadges === 4 && index === 1):
-			return 'even-2';
-		case totalNumberOfBadges === 2 && index === 1 || (totalNumberOfBadges === 4 && index === 2):
-			return 'even-3';
-		case totalNumberOfBadges === 4 && index === 3:
-			return 'even-4';
-		default:
-			return "center"
-	}
-}
 
 export interface CardProps
 	extends React.ButtonHTMLAttributes<HTMLDivElement>,
@@ -111,16 +90,14 @@ export default function CardComponent({ card, variant }: CardProps) {
 					<div className="relative h-full bg-card-portrait bg-no-repeat bg-cover bg-center" />
 					{wip ? <p className="absolute top-20 left-10 text-white text-shadow-black flex text-3xl">Work in Progress</p> : null}
 				</div>
-
+				<span className="col-start-2 relative w-full h-full z-20 flex justify-center">
+					{badges.map((badge, i) => <span key={badge.description} className={badgeVariants({ position: i + 1 <= 4 && i + 1 > 1 ? i + 1 as 1 | 2 | 3 | 4 : 1 })}>
+						<badge.icon className="m-auto" />
+					</span>)}
+				</span>
 				<h2 className="col-span-3 relative bg-center flex justify-center text-center w-full h-full bg-no-repeat bg-cover text-2xl font-semibold text-foreground bg-ribbon">
 					<CurvedText size={headingSize} text={title} />
 				</h2>
-				<span className="col-start-2 relative w-full h-full z-20 flex justify-center">
-					{badges.map((badge, i) => <span key={i} className={badgeVariants({ position: getBadgePosition(i, badges.length) })}>
-						<badge.icon className="m-auto" />
-					</span>)}
-
-				</span>
 				<div className="relative col-start-2 text-center bg-card-description bg-no-repeat h-[102%] -translate-y-[1.2rem] pt-6 px-4 flex flex-col gap-2">
 					{description.map((paragraph) =>
 						<p key={typeof paragraph === "string" ? paragraph : paragraph.key} className={cardDescriptionVariants({ variant: descriptionSize })} > {paragraph}</p>
